4bace3592582526090c95ec3dc3cf8e51a3a8073
IRONDESK <zerozerobase@gmail.com>
2022-09-03 14:04:26 +0900

    :tada: initial setting

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next"
+import styled from "@emotion/styled"
+
+const Home: NextPage = () => {
+  return <Main>Main</Main>
+}
+
+const Main = styled.main``
+
+export default Home
