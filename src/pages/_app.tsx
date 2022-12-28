7d83364cde408cbe374737daf6f49ba0390528b2
IRONDESK <zerozerobase@gmail.com>
2022-12-29 01:46:56 +0900

    :recycle: SWR fetcher 전역 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
-import "../styles/globals.css"
-import { Layout } from "../components/Layouts"
-import type { AppProps } from "next/app"
+import "../styles/globals.css";
+import { Layout } from "../components/Layouts";
+import type { AppProps } from "next/app";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
